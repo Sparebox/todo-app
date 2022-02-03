7acc55403fda8aec5d8a805d9f354929967eebbd
Oskari Ojamaa <eeosoj@utu.fi>
2022-02-03 18:50:18 +0200

    Added styling and fixed bugs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 const notePlaceholder = "Lisää uusi tehtävä...";
+const defaultTodo = {value: "", isCrossedOut: false};
 
 function App() {
   return (
@@ -31,11 +32,14 @@ class TodoTable extends React.Component {
   }
 
   handleAddTodo() {
-    const newTodo = this.state.newTodo.trim();
-    const todos = this.state.todos.slice();
-    const index = todos.indexOf(newTodo);
-    if(newTodo.length === 0 || index != -1) // If new todo is empty or is already in the list
+    const newTodo = {...defaultTodo, value: this.state.newTodo.trim()};
+    const todos = this.state.todos;
+    if(newTodo.value.length === 0) // If new todo is empty
       return;
+    for(const todo of todos) {
+      if(newTodo.value === todo.value) // if new todo is already in the list
+        return;
+    }
     todos.push(newTodo);
     this.setState({
       newTodo: "",
@@ -44,10 +48,9 @@ class TodoTable extends React.Component {
   }
 
   handleRemoveTodo(todo) {
-    const todos = this.state.todos.slice();
-    const index = todos.indexOf(todo);
-    todos.splice(index, 1);
-    this.setState({todos: todos});
+    const todos = this.state.todos;
+    const result = todos.filter(t => t.value !== todo.value);
+    this.setState({todos: result});
   }
 
   handleRemoveAll() {
@@ -55,16 +58,23 @@ class TodoTable extends React.Component {
   }
 
   handleSort() {
-    const todos = this.state.todos.slice();
+    const todos = this.state.todos;
     todos.sort((a,b) => {
-      return a.toLowerCase().charCodeAt(0) - b.toLowerCase().charCodeAt(0) // Sort alphabetically and ignore upper cases
+      return a.value.toLowerCase().charCodeAt(0) - b.value.toLowerCase().charCodeAt(0) // Sort alphabetically and ignore upper cases
     }); 
     this.setState({todos: todos});
   }
 
-  handleDoUndo(e) {
-    const className = e.currentTarget.className;
-    e.currentTarget.className = className === "Todo" ? "Todo-CrossedOut" : "Todo";
+  handleDoUndo(todo) {
+    const todos = this.state.todos;
+    const result = todos.map((t) => {
+      if(t.value === todo.value) {
+        return {...t, isCrossedOut: !t.isCrossedOut}; // Toggle crossed out state
+      } else {
+        return t;
+      }
+    });
+    this.setState({todos: result});
   }
 
   render() {
@@ -81,7 +91,7 @@ class TodoTable extends React.Component {
           onClickRemove={this.handleRemoveTodo}
           onClickDoUndo={this.handleDoUndo}
         />
-        <Buttons 
+        <Buttons
           onClickRemoveAll={this.handleRemoveAll}
           onClickSort={this.handleSort}
         />
@@ -93,13 +103,14 @@ class TodoTable extends React.Component {
 function TodoForm(props) {
   return (
     <div>
-      <input 
+      <input
+        className="TodoForm"
         type="text"
         placeholder={notePlaceholder}
         value={props.newTodo}
         onChange={props.onChangeText}
       />
-      <button type="button" onClick={props.onClickAdd}>
+      <button className="Buttons" type="button" onClick={props.onClickAdd}>
         +
       </button>
     </div>
@@ -109,35 +120,58 @@ function TodoForm(props) {
 
 function TodoList(props) {
   const todoItems = props.todos.map((todo) =>
-    <li className="Todo" key={todo} onClick={(e) => props.onClickDoUndo(e)}>
-      {todo}
-      <button type="button" onClick={() => props.onClickRemove(todo)}>
-        X
-      </button>
-    </li>
+    <TodoItem
+      key={todo.value}
+      todo={todo}
+      onClickDoUndo={props.onClickDoUndo}
+      onClickRemove={props.onClickRemove}
+    />
   );
   return (
-    <ul>
+    <ul className="TodoList">
       {todoItems}
     </ul>
   );
   
 }
 
+function TodoItem(props) {
+  let style = "Todo";
+  if(props.todo.isCrossedOut)
+    style = style + " TodoCrossedOut";
+  return (
+    <div>
+      <li className={style} onClick={() => props.onClickDoUndo(props.todo)}>
+      {props.todo.value}
+      </li>
+      <button className="TodoButton" type="button" onClick={() => props.onClickRemove(props.todo)}>
+        X
+      </button>
+    </div>
+    
+  );
+}
+
 function Buttons(props) {
   return (
     <div>
-      <button type="button" onClick={props.onClickSort}>
+      <button
+        type="button"
+        className="Buttons"
+        onClick={props.onClickSort}
+      >
         Lajittele A-Ö
       </button>
-      <button type="button" onClick={props.onClickRemoveAll}>
+      <button
+        type="button"
+        className="Buttons"
+        onClick={props.onClickRemoveAll}
+      >
         Poista kaikki
       </button>
     </div>
   );
 
 }
-
-
 
 export default App;
